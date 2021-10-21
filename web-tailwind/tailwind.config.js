c826a697893104247f3b635b70261111a30e1933
Misael Aguas Jimenez <missael133@gmail.com>
2021-10-21 16:58:44 -0500

    Implement banner

--- web-tailwind/tailwind.config.js
+++ web-tailwind/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
 			},
 			backgroundImage: {
 				'body-pattern': "url('../img/pattern.png')",
+				'banner': "url('../img/bakbaner.png')",
+			},
+			backgroundPosition: {
+				'banner-position' : "-300px -200px",
 			},
 			colors: {
 				primary: '#fffff',
@@ -20,6 +24,8 @@ module.exports = {
 				'spin-low': 'spin 2s linear infinite',
 				'from-bellow': 'fromBellow 500ms linear',
 				'from-right': 'fromRight 300ms linear',
+				'bg-banner': 'backBanner 10s linear',
+				'text-banner': 'showBannerText 10s linear',
 			},
 			keyframes: {
 				fromBellow:{
@@ -30,6 +36,32 @@ module.exports = {
 				fromRight:{
 					'0%': { transform: 'translateX(200%)'},
 					'100%': { transform: 'translateX(0%)'}
+				},
+				backBanner:{
+					'0%': { 'background-position': '0px 0px'},
+					'100%': { 'background-position': '-300px -200px'}
+				},
+				showBannerText:{
+					'0%': { 
+						transform: 'translateX(-600%) scale(5,5)',
+						opacity: 1
+					},
+					'50%': { 
+						transform: 'translateX(600%) scale(5,5)',
+						opacity: 1
+					},
+					'75%': { 
+						transform: 'translateX(-600%) scale(5,5)',
+						'text-shadow': 'none',
+						color: 'transparent',
+						opacity: 0
+					},
+					'100%': { 
+						transform: 'translateX(0%) scale(1,1)',
+						'text-shadow': '2px 2px 1px black',
+						color: 'white',
+						opacity: 0
+					},
 				},
 			},
 		},
