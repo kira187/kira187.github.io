9a1b50d5d5ab3ce52254fc1d05d7865799dd0ed7
Misael Aguas Jimenez <missael133@gmail.com>
2021-10-21 09:33:35 -0500

    implement navbar

---
+++ web-tailwind/tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  	purge: [],
+  	darkMode: false,
+  	theme: {
+    	extend: {
+			screens: {
+				'xs': {'max': '768px'}
+			},
+			backgroundImage: {
+				'body-pattern': "url('../img/pattern.png')",
+			},
+			colors: {
+				primary: '#fffff',
+				blue: {
+					light: '#37bcf9',
+				  	dark: '#0370b9',
+				}
+			},
+			animation: {
+				'spin-low': 'spin 2s linear infinite',
+				'from-bellow': 'fromBellow 500ms linear',
+				'from-right': 'fromRight 300ms linear',
+			},
+			keyframes: {
+				fromBellow:{
+					'0%': { transform: 'translateY(0%)'},
+					'50%': { transform: 'translateY(200%)'},
+					'100%': { transform: 'translateY(0%)'}
+				},
+				fromRight:{
+					'0%': { transform: 'translateX(200%)'},
+					'100%': { transform: 'translateX(0%)'}
+				},
+			},
+		},
+  	},
+  	variants: {
+		animation: ['responsive', 'hover','group-hover'],
+        animate: ['responsive', 'hover', 'group-hover'],
+  	},
+  	plugins: [],
+}
